4dae297a76ed4ab5bb040f7fb1661d518335ea9c
Sander Shi <sander.hacker@hotmail.com>
2017-04-03 15:29:40 -0400

    Updated to Angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
@@ -34,6 +35,7 @@ import { DeviceService } from './resources/device.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     JsonpModule,
